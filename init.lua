48471fcadcbd29704c33db2c7f49256feeaab9fe
Mike Watkins <github@mikewatkins.ca>
2022-12-12 22:29:18 -0800

    cleanup

--- init.lua
+++ init.lua
@@ -43,9 +43,7 @@ if vim.fn.has "nvim-0.8" ~= 1 or vim.version().prerelease then
     vim.schedule(function() vim.notify("Unsupported Neovim Version! Must be 0.8 or greater", "error") end)
 else
     require('core')
-    -- lua/user.lua is not part of this repo and can be used as a per-user override should
-    -- anyone else be using my configuration and need to customize their own keymaps or vim.opts.
-    -- copy lua/user-example.lua to lua/user.lua and customize as needed.
+    -- lua/user.lua is not part of this repo and can be used as a per-user override
     local user_overrides = vim.fn.stdpath('config') .. '/lua/user.lua'
     local f = io.open(user_overrides)
     if f then
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,5 +1,5 @@
 
 require('core.plugins') -- should be first, as it initializes packer if not installed
-require('core.config')
-require('core.options')
-require('core.mapping')
+require('core.config') -- configuration for plugins that require more than a simple setup()
+require('core.options') -- neovim options
+require('core.mapping') -- key maps, mostly driven by Whichkey
