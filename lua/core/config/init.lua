28b18d69e2e5d34ca4e6d78ef53d38c0b76a9256
Mike Watkins <github@mikewatkins.ca>
2022-12-14 07:08:24 -0800

    simplify: move telescope maps to mappings

--- lua/core/config/init.lua
+++ lua/core/config/init.lua
@@ -1,13 +1,14 @@
 -- nvim/lua/core/config/init.lua
 
--- for simple one-liners, include them here:
+-- for simple one-liners, if needed, include them here:
 require("bufferline").setup{}
 require('Comment').setup()
 require("nvim-autopairs").setup{}
 require("nvim-lastplace").setup{}
-require('which-key').setup() -- mappings are (mostly) in core/mappings.lua
+require('telescope').setup()
+require('which-key').setup() -- mappings are in core/mappings.lua
 
--- More complex config/setup go in individual files under config
+-- More complex config/setup go in individual files under core/config/...
 require('core.config.cmp') -- lsp related
 require('core.config.colorizer')
 require('core.config.gitsigns')
@@ -15,5 +16,4 @@ require('core.config.indent-blankline')
 require('core.config.lsp')
 require('core.config.lualine')
 require('core.config.nvim-tree')
-require('core.config.telescope')
 require('core.config.treesitter')
