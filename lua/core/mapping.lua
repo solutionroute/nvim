66334ea90f21d0a038f7fc9c33c187b3b770692f
Mike Watkins <github@mikewatkins.ca>
2022-12-14 17:54:53 -0800

    remove ;; map

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -12,7 +12,6 @@ map("n", "<Tab>", "<cmd>bnext<CR>", {desc = "Buffer Next"})
 map("n", "<S-Tab>", "<cmd>bprevious<CR>", {desc = "Buffer Previous"})
 map("n", "<C-s>", "<cmd>write<CR>", {desc = "write buffer"}) -- same as <Leader>w
 map("i", "<C-s>", "<esc><cmd>write<CR>", {desc = "write buffer"}) -- same as <Leader>w
-map("i", ";;", "<esc>:", {desc = "esc: in insert mode"}) -- saves Esc: to get to cmd mode, ymmv
 
 -- or the which-key way (these all for <Leader>)
 local wk = require("which-key")
