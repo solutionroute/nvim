a555818a3feb69a836e77318ce92e316a0b63999
Mike Watkins <github@mikewatkins.ca>
2022-12-17 10:46:26 -0800

    docs

--- init.lua
+++ init.lua
@@ -2,10 +2,17 @@
 --
 -- An understandable Neovim configuration aiming for simplicity but not at the
 -- expense of capabilities, meant for code and general purpose editing.
+--
+-- To override defaults:
 -- 
+--   Copy nvim/user_example.lua -> nvim/user.lua, and within that file set your
+--   preferred colour scheme, options, and mappings.
+--
 -- New to Lua/Lua in nvim? Check out: 
+--
 --   https://github.com/nanotee/nvim-lua-guide
 --   https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/
+
 
 -- nvim-tree: disable netrw at startup to avoid race condition 
 vim.g.loaded_netrw = 1
@@ -13,15 +20,11 @@ vim.g.loaded_netrwPlugin = 1
 
 local min_minor = 8 -- nvim-0.8+
 
--- nvim-0.8+ or nightly 
 if vim.version().minor >= min_minor or vim.version().prerelease then
   -- core/init.lua contains packer init and loads other core modules when not bootstrapping
   require('core')
 
-  -- To override defaults:
-  -- copy nvim/user_example.lua -> nvim/user.lua, and set your preferred colour
-  -- scheme, options, and mappings.
-  -- only run if not bootstrapping
+  -- only load user customizaions when not bootstrapping
   if package.loaded['core.options'] then
     local user_config = vim.fn.stdpath('config') .. '/user.lua'
     local f = io.open(user_config)
