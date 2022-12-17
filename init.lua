19bdddd5acb596d7a015d4d18f39f33317c35a2b
Mike Watkins <github@mikewatkins.ca>
2022-12-17 09:26:20 -0800

    source, not require as mod, optional user.lua

--- init.lua
+++ init.lua
@@ -1,22 +1,25 @@
 -- https://github.com/solutionroute/nvim 
--- A simple and understandable Neovim configuration meant for code and general
--- purpose editing.
+-- An understandable Neovim configuration meant for code and general purpose
+-- editing.
 
--- nvim-tree: disable at start to avoid race condition 
+-- nvim-tree: disable netrw at start to avoid race condition 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 if vim.fn.has "nvim-0.8" ~= 1 or vim.version().prerelease then
   vim.schedule(function() vim.notify("Unsupported Neovim Version! Must be 0.8 or greater", "error") end)
 else
-  require('core')
-  -- nvim/lua/user.lua is not part of this repo; create & use as your override for
-  -- package default options and mappings; see or copy user_example.lua
-  local user_overrides = vim.fn.stdpath('config') .. '/lua/user.lua'
-  local f = io.open(user_overrides)
+  -- all modules in core are loaded by plugins after packer has been initialized
+  require('core.plugins')
+
+  -- To override defaults:
+  -- copy user_example.lua -> user.lua, and set within it your preferred colour
+  -- scheme, options, and mappings.
+  local user_config = vim.fn.stdpath('config') .. '/user.lua'
+  local f = io.open(user_config)
   if f then
     f:close()
-    require('user')
+    vim.cmd('luafile '..user_config)
   end
 end
 
