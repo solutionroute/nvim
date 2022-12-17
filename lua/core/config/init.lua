438e04fe77e00169d26b8b7855ac89ac0d042f6c
Mike Watkins <github@mikewatkins.ca>
2022-12-16 20:57:47 -0800

    mv to lsp-zero for lsp config

--- lua/core/config/init.lua
+++ lua/core/config/init.lua
@@ -1,19 +1,9 @@
 -- nvim/lua/core/config/init.lua
--- for simple one-liners, if needed, include them here:
-require("lsp_signature").setup{doc_lines=0, hint_enable=false}
-require("nvim-autopairs").setup{}
-require("nvim-lastplace").setup{}
-require('telescope').setup()
-require('which-key').setup() -- mappings are in core/mappings.lua
-
--- More complex config/setup go in individual files under core/config/...
+-- simple configs are found in nvim/lua/core/plugins.lua
 require('core.config.bufferline')
-require('core.config.cmp')
 require('core.config.comment')
-require('core.config.colorizer')
 require('core.config.gitsigns')
-require('core.config.indent-blankline')
-require('core.config.lsp')
 require('core.config.lualine')
 require('core.config.nvim-tree')
 require('core.config.treesitter')
+require('core.config.zerolsp')
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -43,62 +43,7 @@ packer.init {
 
 return packer.startup(function(use)
   use 'wbthomason/packer.nvim' -- packer manages itself
-  -- misc functions used by some plugins
-  use 'nvim-lua/plenary.nvim'
-  -- file browsing 
-  use 'nvim-tree/nvim-tree.lua'
-  -- icons used by nvim-tree, tabs/bufferline/lualine
-  use 'nvim-tree/nvim-web-devicons'
-  -- restore cursor position in file
-  use 'ethanholz/nvim-lastplace'
-  -- quick find... anything
-  use {'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim'}
-  -- tabs/buffers
-  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-web-devicons'}
-  -- status line
-  use {'nvim-lualine/lualine.nvim', requires = 'nvim-web-devicons'}
-  -- dynamic menus based on mappings
-  use 'folke/which-key.nvim'
-  -- code highlighting and more
-  use 'nvim-treesitter/nvim-treesitter'
-  -- treesitter dependent: 
-  use {'JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter'} -- Context based commenting
-  use {'windwp/nvim-ts-autotag', after = 'nvim-treesitter'} -- like autopairs but for html and more, config in treesitter
-  use { "RRethy/vim-illuminate", after = 'nvim-treesitter'} -- highlight like terms (uses ts/lsp/regex)
-  -- indent lines, configured to highlight current block
-  use 'lukas-reineke/indent-blankline.nvim'
-  -- highlight rgb colour strings like #ffcc33
-  use 'NvChad/nvim-colorizer.lua'
-  -- match braces, brackets and such
-  use 'windwp/nvim-autopairs'
-  -- commenting
-  use 'numToStr/Comment.nvim'
-  -- git integration
-  use {'lewis6991/gitsigns.nvim', requires = 'folke/which-key.nvim'}
-  -- LSP provision and completion; configured in config.lsp.lua
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "jay-babu/mason-null-ls.nvim",
-  }
-  -- symbols (vscode-like) for Lsp elements in completion dialogs
-  use 'onsails/lspkind.nvim'
-  -- completion engine, requires snippet engine and sources
-  use 'hrsh7th/nvim-cmp'
-  -- completion sources
-  use {'hrsh7th/cmp-nvim-lsp', requires = 'nvim-cmp',}
-  use {'hrsh7th/cmp-buffer', after = 'nvim-cmp',}
-  use {'hrsh7th/cmp-path', after = 'nvim-cmp',}
-  use {'hrsh7th/cmp-cmdline', after = 'nvim-cmp',}
-  use {'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp'}
-  use {'saadparwaiz1/cmp_luasnip', wants = 'friendly-snippets', requires = 'LuaSnip'}
 
-  -- snippets
-  use 'L3MON4D3/LuaSnip'
-  use {'rafamadriz/friendly-snippets', opt = true}
-  -- function signature display as you type
-  use {'ray-x/lsp_signature.nvim'}
   -- colour schemes
   use 'lunarvim/darkplus.nvim' -- default
   use 'EdenEast/nightfox.nvim'
@@ -106,6 +51,89 @@ return packer.startup(function(use)
   use 'folke/tokyonight.nvim'
   use {'catppuccin/nvim', as = 'catppuccin'}
   use 'marko-cerovac/material.nvim'
+
+  -- user interface
+  -- icons used by nvim-tree, tabs/bufferline/lualine
+  use 'nvim-tree/nvim-web-devicons'
+  -- tabs/buffers
+  use {'akinsho/bufferline.nvim', tag = 'v3.*', requires = 'nvim-web-devicons'}
+  -- status line
+  use {'nvim-lualine/lualine.nvim', requires = 'nvim-web-devicons'}
+  -- highlight rgb colour strings like #ffcc33
+  use {'NvChad/nvim-colorizer.lua', config = function() require('colorizer').setup{} end }
+  -- dynamic menus based on mappings
+  use {'folke/which-key.nvim', config = function() require('which-key').setup{} end }
+  -- restore cursor position in file
+  use {'ethanholz/nvim-lastplace', config = function() require('nvim-lastplace').setup{} end }
+  -- commenting
+  use 'numToStr/Comment.nvim'
+  -- automatic session save
+  use({ "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    module = "persistence",
+    config = function()
+    require("persistence").setup()
+    end,
+  })
+  -- match braces, brackets and such
+  use {'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup{} end }
+  -- indent lines, configured to highlight current block
+  use {'lukas-reineke/indent-blankline.nvim',
+    config = function()
+      require('indent_blankline').setup{
+        show_current_context = true,
+        show_current_context_start = false,
+      }
+    end
+  }
+  -- git integration
+  use('tpope/vim-fugitive')
+  use {'lewis6991/gitsigns.nvim', requires = 'folke/which-key.nvim'}
+  -- file browsing 
+  use 'nvim-tree/nvim-tree.lua'
+  -- quick find... anything
+  use {'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim', config = function() require('telescope').setup{} end }
+
+  -- Treesitter & LSP Support
+  -- code highlighting and more
+  use {'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+  -- treesitter dependent, see treesitter config
+  use {'JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter'} -- Context based commenting
+  use {'windwp/nvim-ts-autotag', after = 'nvim-treesitter'} -- like autopairs but for html and more, config in treesitter
+  use { "RRethy/vim-illuminate", after = 'nvim-treesitter'} -- highlight like terms (uses ts/lsp/regex)
+
+  -- Zero (almost) configuration of LSP
+  use {'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+  -- function signature display as you type
+  use {'ray-x/lsp_signature.nvim',
+    config = function()
+      require('lsp_signature').setup{doc_lines=0, hint_enable=false}
+    end
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
