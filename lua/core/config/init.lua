0704eed7c6e565c2f9f3cc7e3a0aa897a6d00d3e
Mike Watkins <github@mikewatkins.ca>
2022-12-17 17:23:53 -0800

    switch to neo-tree, remove buffer tabs

--- lua/core/config/init.lua
+++ lua/core/config/init.lua
@@ -1,9 +1,6 @@
 -- nvim/lua/core/config/init.lua
 -- simple configs are found in nvim/lua/core/plugins.lua
-require('core.config.bufferline')
 require('core.config.comment')
 require('core.config.gitsigns')
-require('core.config.lualine')
-require('core.config.nvim-tree')
 require('core.config.treesitter')
 require('core.config.zerolsp')
