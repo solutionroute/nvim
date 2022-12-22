dc2d871b5be36cc4abd69d7a6145bc50f50df0d3
Mike Watkins <github@mikewatkins.ca>
2022-12-22 05:46:31 -0800

    bg transparent; with alacritty slightly trans too

--- lua/core/config/colorscheme.lua
+++ lua/core/config/colorscheme.lua
@@ -4,37 +4,37 @@
 -- Material
 vim.g.material_style = "darker"
 require('material').setup({
-  contrast = {
-      cursor_line = true
-  },
-  styles = {
-      comments = {italic = true,},
-  },
-  plugins = {
-    'gitsigns',
-    'indent-blankline',
-    'nvim-cmp',
-    'nvim-tree',
-    'nvim-web-devicons',
-    'telescope',
-    'which-key',
-  },
+    contrast = {
+        cursor_line = true
+    },
+    styles = {
+        comments = { italic = true, },
+    },
+    plugins = {
+        'gitsigns',
+        'indent-blankline',
+        'nvim-cmp',
+        'nvim-tree',
+        'nvim-web-devicons',
+        'telescope',
+        'which-key',
+    },
 })
 
 -- Nightfox
 require('nightfox').setup({
-  options = {
-    transparent = false,
-    styles = {
-      comments = "italic",
-      -- keywords = "bold",
-      -- types = "italic,bold",
+    options = {
+        transparent = true,
+        styles = {
+            comments = "italic",
+            -- keywords = "bold",
+            -- types = "italic,bold",
+        }
+    },
+    palettes = {
+        nordfox = { bg1 = "#20242c", -- #2e3440 is the original
+        },
     }
-  },
-  palettes = {
-    -- #2e3440 is the original
-    nordfox = {bg1="#20242c",},
-  }
 })
 
 -- system default colourscheme; see nvim/user-example for others.
