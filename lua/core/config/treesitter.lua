24d6fba7b0be32d1e4a821749b4e701c575d0bf4
Mike Watkins <github@mikewatkins.ca>
2023-01-06 15:35:39 -0800

    install all ts parsers

--- lua/core/config/treesitter.lua
+++ lua/core/config/treesitter.lua
@@ -1,35 +1,35 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 local ok, m = pcall(require, "nvim-treesitter.configs")
 if not ok then
-  return
+	return
 end
 
 m.setup {
-  -- A list of parser names, or "all"; note, some parsers require Nodejs / npm - see our README.md
-  -- ensure_installed = "all",
-  ensure_installed = {
-    "bash", "c", "css", "go", "gomod", "help", "html", "javascript", "json", "lua",
-    "markdown", "make", "python", "svelte", "toml", "typescript", "vim", "vue", "yaml" },
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  -- configuration for certain other plugins relying on tree-sitter
-  autopairs = {
-   enable = true,
-  },
-  -- https://github.com/windwp/nvim-ts-autotag
-  autotag = {
-    enable = true,
-    incremental_selection = { enable = true },
-    indent = { enable = false },
-  },
-  -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+	-- A list of parser names, or "all"; note, some parsers require Nodejs / npm - see our README.md
+	ensure_installed = "all",
+	-- ensure_installed = {
+	--   "bash", "c", "css", "go", "gomod", "help", "html", "javascript", "json", "lua",
+	--   "markdown", "make", "python", "svelte", "toml", "typescript", "vim", "vue", "yaml" },
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	-- configuration for certain other plugins relying on tree-sitter
+	autopairs = {
+		enable = true,
+	},
+	-- https://github.com/windwp/nvim-ts-autotag
+	autotag = {
+		enable = true,
+		incremental_selection = { enable = true },
+		indent = { enable = false },
+	},
+	-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 }
