dbcf881faa571901d7094c2fba3a8df05a986f18
Mike Watkins <github@mikewatkins.ca>
2023-01-06 15:55:46 -0800

    nicer lsp icons

--- lua/core/config/zerolsp.lua
+++ lua/core/config/zerolsp.lua
@@ -1,28 +1,28 @@
 local ok, m = pcall(require, "lsp-zero")
 if not ok then
-  return
+	return
 end
 
 vim.opt.signcolumn = 'yes' -- Reserve space for diagnostic icons
 m.preset('recommended')
 
 m.ensure_installed({
-  'eslint',
-  'gopls',
-  'sqlls',
-  'tsserver',
-  'sumneko_lua',
+	'eslint',
+	'gopls',
+	'sqlls',
+	'tsserver',
+	'sumneko_lua',
 })
 
 m.nvim_workspace()
 
 m.set_preferences({
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+	sign_icons = {
+		error = '✘',
+		warn = '▲',
+		hint = '⚑',
+		info = ''
+	}
 })
 
 m.setup()
